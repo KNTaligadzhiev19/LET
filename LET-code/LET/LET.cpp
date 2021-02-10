6528fe8bd848b625fd12bc3f10d8282927160ddb
Radoslav Ivanov <RGIvanov19@codingburgas.bg>
2021-02-10 22:21:29 +0200

    Add comments for displayLogo and displayStartPage functions

--- LET-code/LET/LET.cpp
+++ LET-code/LET/LET.cpp
@@ -3,6 +3,7 @@
 #include <Windows.h>
 using namespace std;
 
+//Draws the abbreviation of the application
 void displayLogo()
 {
 	printf("\t\t\t\t\t   Welcome to LET (Learn English Tenses)!");
@@ -15,17 +16,21 @@ void displayLogo()
 	printf("\n\t\t\t\t|___________\t        |-------------\t\t      |");
 }
 
+//Displays the modes of the application
 void displayStartPage()
 {
 	displayLogo();
 	printf("\n\n\n");
 
+	//Draws the line under the abbreviation
 	for (int i = 0; i < 120; i++)
 	{
 		cout << "_";
+		//Slows down the drawing of the line
 		Sleep(15);
 	}
 
+	//Displays the two modes of the application
 	printf("\n\n\n\t\t    Enter 1 To Practise English Tenses\t\t     Enter 2 To Play Guess The Word");
 	printf("\n\n\n\t\t\t\t\t\t     Enter Your Choice: ");
 }
