405231765b60d96fedcc80130b1bfbbca46970cf
Veselin Stoqnov <VNStoyanov19@codingburgas.bg>
2021-02-11 12:52:28 +0200

    Add the figure and result of the Guess the word game

--- LET-code/LET/LET.cpp
+++ LET-code/LET/LET.cpp
@@ -35,6 +35,88 @@ void displayStartPage()
 	printf("\n\n\n\t\t\t\t\t\t     Enter Your Choice: ");
 }
 
+void displayTheTable(string mutualChoice)
+{
+	printf("\n\t\t______________________");
+	printf("\n\t\t|\t\t     |");
+	printf("\n\t\t|\t\t     |");
+	cout << "\n\t\t|     " << mutualChoice << "       |";
+	printf("\n\t\t|\t\t     |");
+	printf("\n\t\t|____________________|");
+	printf("\n\n\n\n\t\tEnter The Word: ");
+}
+
+void displayTheDeadBoyHead()
+{
+	printf("\t\t\t\t\t   Welcome to LET (Learn English Tenses)!");
+	printf("\n\n\t\t\t\t\t\t\t\t\t  _");
+	printf("\n\t\t\t\t\t\t\t\t\t / \\");
+	printf("\n\t\t\t\t\t\t\t\t\t/   \\");
+	printf("\n\t\t\t\t\t\t\t\t       |     |");
+
+	for (int i = 0; i < 2; i++) printf("\n\t\t\t\t\t\t\t\t       |     |");
+
+	printf("\n\t\t\t\t\t\t\t\t        \\   /");
+	printf("\n\t\t\t\t\t\t\t\t         \\_/");
+}
+
+void displayTheDeadBoyBody(string mutualChoice)
+{
+	displayTheDeadBoyHead();
+
+	printf("\n\t\t______________________\t\t\t\t\t  |");
+	printf("\n\t\t|\t\t     |\t\t\t\t\t /|\\");
+	printf("\n\t\t|\t\t     |\t\t\t\t\t/ | \\");
+	cout << "\n\t\t|     " << mutualChoice << "       |" << "\t\t\t\t       /  |  \\";
+	printf("\n\t\t|\t\t     |\t\t\t\t      /   |   \\");
+	printf("\n\t\t|____________________|\t\t\t\t     /    |    \\");
+}
+
+void displayTheDeadBoyLegs(string mutualChoice)
+{
+	system("cls");
+
+	displayTheDeadBoyBody(mutualChoice);
+
+	printf("\n\t\t\t\t\t\t\t\t\t  |");
+	printf("\n\t\t\t\t\t\t\t\t\t / \\");
+	printf("\n\t\t\t\t\t\t\t\t\t/   \\");
+	printf("\n\t\t\t\t\t\t\t\t       /     \\");
+	printf("\n\t\t\t\t\t\t\t\t      /       \\");
+	printf("\n\t\t\t\t\t\t\t\t     /         \\");
+}
+
+void displayTheResultOfGuessTheWord(string mutualChoice)
+{
+	system("cls");
+
+	printf("\t\t\t\t\t   Welcome to LET (Learn English Tenses)!");
+	printf("\n\n\t\t\t\t  _________________________________________________________");
+
+	for (int i = 0; i < 2; i++) printf("\n\t\t\t\t |\t\t\t\t\t\t\t   |");
+
+	printf("\n\t\t\t\t |\t\t      Thanks For Playing!\t\t   |");
+	printf("\n\t\t\t\t |\t\t\t\t\t\t\t   |");
+	printf("\n\t\t\t\t |_________________________________________________________|");
+
+	for (int i = 0; i < 2; i++) printf("\n\t\t\t\t |\t\t\t\t\t\t\t   |");
+
+	printf("\n\t\t\t\t |\t  \t Congratulations Your Score Is:  \t   |");
+
+	for (int i = 0; i < 4; i++) printf("\n\t\t\t\t |\t\t\t\t\t\t\t   |");
+
+	cout << "\n\t\t\t\t |\t  \t\t   " << points << " Points\t\t\t   |";
+
+	for (int i = 0; i < 4; i++) printf("\n\t\t\t\t |\t\t\t\t\t\t\t   |");
+
+	printf("\n\t\t\t\t |\t  \t Congratulations Your Score Is:  \t   |");
+	printf("\n\t\t\t\t |\t\t\t\t\t\t\t   |");
+	printf("\n\t\t\t\t |_________________________________________________________|\n\n\n");
+	printf("\t\t\t\t\t\t     Press Enter To Continue: ");
+
+	getline(cin, mutualChoice);
+}
+
 int main()
 {
 	displayStartPage();
