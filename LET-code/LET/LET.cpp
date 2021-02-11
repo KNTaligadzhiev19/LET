75bd363d2d43e88af8326da28d4dce3a70e5d806
Veselin Stoqnov <VNStoyanov19@codingburgas.bg>
2021-02-11 19:28:56 +0200

    Add menu for the exercises

--- LET-code/LET/LET.cpp
+++ LET-code/LET/LET.cpp
@@ -189,6 +189,36 @@ void displayTheResultOfGuessTheWord(string mutualChoice)
 	getline(cin, mutualChoice);
 }
 
+void displayMenu()
+{
+	system("cls");
+
+	printf("\t\t\t\t\t   Welcome to LET (Learn English Tenses)!");
+	printf("\n\n\n\t\t\t\t  _________________________________________________________");
+	printf("\n\t\t\t\t |\t\t\t\t\t\t\t   |");
+	printf("\n\t\t\t\t |\t\t\t\t\t\t\t   |");
+	printf("\n\t\t\t\t |\t\t\t    Menu\t\t\t   |");
+	printf("\n\t\t\t\t |\t\t\t\t\t\t\t   |");
+	printf("\n\t\t\t\t |_________________________________________________________|");
+	printf("\n\t\t\t\t |\t\t\t\t\t\t\t   |");
+	printf("\n\t\t\t\t |\t\t\t\t\t\t\t   |");
+	printf("\n\t\t\t\t |\t  Enter 1 to practise Present Simple Tense\t   |");
+	printf("\n\t\t\t\t |\t\t\t\t\t\t\t   |");
+	printf("\n\t\t\t\t |\t  Enter 2 to practise Present Continuous Tense\t   |");
+	printf("\n\t\t\t\t |\t\t\t\t\t\t\t   |");
+	printf("\n\t\t\t\t |\t  Enter 3 to practise Past Simple Tense\t\t   |");
+	printf("\n\t\t\t\t |\t\t\t\t\t\t\t   |");
+	printf("\n\t\t\t\t |\t  Enter 4 to practise Past Continuous Tense\t   |");
+	printf("\n\t\t\t\t |\t\t\t\t\t\t\t   |");
+	printf("\n\t\t\t\t |\t  Enter 5 to practise Present Perfect Tense\t   |");
+	printf("\n\t\t\t\t |\t\t\t\t\t\t\t   |");
+	printf("\n\t\t\t\t |\t  Enter 6 to practise Future Simple Tense\t   |");
+	printf("\n\t\t\t\t |\t\t\t\t\t\t\t   |");
+	printf("\n\t\t\t\t |_________________________________________________________|");
+	printf("\n\n\n\t\t\t\t\t\t\tYour Choice: ");
+}
+
+
 int main()
 {
 	displayStartPage();
