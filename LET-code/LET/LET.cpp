9c2298b798f8bf2d792b354a50c45fca45118fa7
Mario Berberov <MCBerberov19@codingburgas.bg>
2021-02-12 12:30:40 +0200

    Add present continuous tense and fix the start page

--- LET-code/LET/LET.cpp
+++ LET-code/LET/LET.cpp
@@ -37,7 +37,8 @@ void displayStartPage()
 
 	//Displays the two modes of the application
 	printf("\n\n\n\t\t    Enter 1 To Practise English Tenses\t\t     Enter 2 To Play Guess The Word");
-	printf("\n\n\n\t\t\t\t\t\t     Enter Your Choice: ");
+	printf("\n\n\n\n\t\t\t\t\t      Enter 3 To Practise Irregular Verbs ");
+	printf("\n\n\n\n\t\t\t\t\t\t      Enter Your Choice: ");
 }
 
 void displayTheTable(string mutualChoice)
@@ -347,6 +348,115 @@ void practisePresentSimpleTenseEx4(string mutualChoice)
 	string answers[10] = { "Does he love to play basketball?" , "Does he go to school?" , "Does she write an e-mail to her best friend?"
 	, "Does he think he is very handsome?" , "Does it usually rain every day here?" , "Does it smell very delicious in the kitchen?"
 	, "Do we go to a gallery every Sunday?" , "Does the sun rise at the east?" , "Does she go to work by car?" , "Do you have a big house?" };
+
+	string condition[1] = { "Enter The Question Form Of The Sentence: " };
+
+	system("cls");
+
+	printf("\t\t\t\t\t   Welcome to LET (Learn English Tenses)!");
+	printf("\n\n\nEx.4 Change The Sentences Into Questions.");
+
+	answerCheckEx(sentence, condition, mutualChoice, answers);
+}
+
+void displayTheUsageOfPresentContinuousTense(string mutualChoice)
+{
+	system("cls");
+
+	printf("\t\t\t\t\t   Welcome to LET (Learn English Tenses)!");
+	printf("\n\n\t\t\t\t\t    The Usage Of Present Continuous Tense.");
+	printf("\n\n\n 1. First, we use the present continuous for things that are happening at the moment of speaking.");
+	printf("\n\n\t- I'm working at the moment.");
+	printf("\n\n\t- Please call back as we are eating dinner now.");
+	printf("\n\n\t- Julie is sleeping.");
+	printf("\n\n\n 2. We can also use this tense for kinds of temporary situations, even if the action isn't happening at this moment.");
+	printf("\n\n\t- John's working in a bar until he finds a job in his field. (He might not be working now.)");
+	printf("\n\n\t- I'm reading a really great book.");
+	printf("\n\n\t- She's staying with her friend for a week.");
+	printf("\n\n\n 3. We can use the present continuous for temporary or new habits.");
+	printf("\n\n\t- He's eating a lot these days.");
+	printf("\n\n\t- She's swimming every morning (she didn't use to do this).");
+	printf("\n\n\t- You're smoking too much.");
+	printf("\t\t\t\t\tPress Enter To Continue: ");
+
+	getline(cin, mutualChoice);
+}
+
+void practisePresentContinuousTenseEx1(string mutualChoice)
+{
+	string sentence[10] = { "1. The boys are ___ (play) football after school.", "2. They are ___ (learn) Spanish now.","3. Why aren't you ___ (do) your homework?",
+		"4. Suzan is ___ (iron) her clothes right now.","5. Mary is at university. She's ___ (study) chemistry.", "6. She is ___ (work) right now.",
+		"7. They aren't ___ (study) English these days.","8. She is constanly ___ (change) her hair color.","9. Children are ___ (study) their lessons.",
+		"10. The man is ___ (talk) on the phone." };
+
+	string answers[10] = { "playing" , "learning" , "doing" , "ironing" , "studying" , "working" , "studying" , "changing" , "studying" , "talking" };
+
+	string condition[1] = { "Enter The Word: " };
+
+	system("cls");
+
+	printf("\t\t\t\t\t   Welcome to LET (Learn English Tenses)!");
+	printf("\n\n\nEx.1 Enter The Word To Complete The Sentence Correctly.");
+
+	answerCheckEx(sentence, condition, mutualChoice, answers);
+}
+
+void practisePresentContinuousTenseEx2(string mutualChoice)
+{
+	string sentence[10] = { "1. A: Where are you going?\n  B: I ___ going to school.",
+	"2. A: What are you doing?\n  B: I am ___ the guitar.","3. A: What are they doing?\n  B: ___ are playing football.",
+	"4. A: ___ we starting?\n  B: Yes, we are.","5. A: Why are you ___?\n  B: Because, I fell terrible.",
+	"6. A: Why ___ they skiing?\n  B: I have no idea.","7. A: Are you studying?\n  B: Yes, I ___.",
+	"8. A: I love reading detective stories.\n  B: Realy? At this moment I ___ reading detective story.",
+	"9. A: Is the girl studying ___?\n  B: Yes, she is.",
+	"10. A: Are you having a bath right now?\n  B: No, I __." };
+
+	string answers[10] = { "a" , "a" , "b" , "a" , "a" , "b" , "a" , "b" , "a" , "b" };
+
+	string chooseAnswer[10]{ "\n\n\ta) am  b) is" , "\n\n\ta) playing  b) play" , "\n\n\ta) We  b) They" , "\n\n\ta) Are  b) Do"
+	, "\n\n\ta) crying  b) cry" , "\n\n\ta) we  b) are" , "\n\n\ta) do  b) does" , "\n\n\ta) do  b) am" , "\n\n\ta) now  b) general"
+	, "\n\n\ta) doesn't b) don't" };
+
+	string condition[1] = { "Enter The Correct Answer (Enter The Letter Only!): " };
+
+	system("cls");
+
+	printf("\t\t\t\t\t   Welcome to LET (Learn English Tenses)!");
+	printf("\n\n\nEx.2 Choose The Correct Answer.");
+
+	answerCheckExMultipleChoice(sentence, condition, mutualChoice, answers, chooseAnswer);
+}
+
+void practisePresentContinuousTenseEx3(string mutualChoice)
+{
+	string sentence[10] = { "1. The boys is playing football now.","2. They are learn Spanish now.",
+	"3. Why aren't you do your homework?","4. Suzan are ironing her clothes right now.",
+	"5. Marry is at university. She is study chemistry.","6. She is works at this moment.",
+	"7. They isn't studying English these days.","8. She is constantly changed her hair color.",
+	"9. Children is studying their lessons.","10. The man is talks on the phone now." };
+
+	string answers[10] = { "are" , "learning" , "doing" , "is" , "studying" , "working" , "aren't" , "changing" , "are" , "talking" };
+
+	string condition[1] = { "Enter The Correct Form Of The Mistake: " };
+
+	system("cls");
+
+	printf("\t\t\t\t\t   Welcome to LET (Learn English Tenses)!");
+	printf("\n\n\nEx.3 Correct The Mistakes (Only The Incorrect Word).");
+
+	answerCheckEx(sentence, condition, mutualChoice, answers);
+}
+
+void practisePresentContinuousTenseEx4(string mutualChoice)
+{
+	string sentence[10] = { "1. They are playing football after school.", "2. They are learning Spanish now.","3. You are doing your homework now.",
+		"4. They are walking in the lawn.","5. Mary is making a cake right now.", "6. He is training at this moment.",
+		"7. It is raining.","8. He is running to the yard.","9. We are meeting in the room.",
+		"10. I am going to school." };
+
+	string answers[10] = { "Are they playing football after school?" , "Are they learning Spanish now?" , "Are you doing your homework now?"
+	, "Are they walking in the lawn?" , "Is Mary making a cake right now?" , "Is he training at this moment?" , "Is it raining?" , "Is he running to the yard?"
+	, "Are we meeting in the room?" , "Am I going to school?" };
 
 	string condition[1] = { "Enter The Question Form Of The Sentence: " };
 
