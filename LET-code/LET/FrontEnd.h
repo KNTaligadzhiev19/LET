5ca34b3af7190d7c6b0c107e679cd13626c675c2
KNTaligadzhiev19 <KNTaligadzhiev19@codingburgas.bg>
2021-03-01 01:59:56 +0200

    Add header files

---
+++ LET-code/LET/FrontEnd.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <Windows.h>
+#include <stdlib.h>
+
+using namespace std;
+
+// Draws the abbreviation of the application
+void displayLogo();
+
+// Displays the modes of the application
+void displayStartPage();
+
+// Displays the table with a random word, that user has to guess
+void displayTheTable(string mutualChoice);
+
+// Displays the head of the boy, when the user makes the first wrong prediction
+void displayTheDeadBoyHead();
+
+// Displays the body of the boy, when the user makes the second wrong prediction
+void displayTheDeadBoyBody(string mutualChoice);
+
+// Displays the legs of the boy, when the user makes the third wrong prediction
+void displayTheDeadBoyLegs(string mutualChoice);
+
+// Displays a table with the score
+void displayTheResultOfGuessTheWord(string mutualChoice, int& points);
+
+// Displays the menu with the options
+void displayMenu();
+
+// Displays a table with the score
+void displayTheResultOfTheExercises(int& points);
+
+// Displays a table with commands
+void irregularVerbsCommandTable(int command);
+
+// Uses for get back on the home page 
+int processMenu(string mutualChoice);
---
+++ LET-code/LET/PractiseExercises.h
@@ -0,0 +1,98 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <Windows.h>
+#include <stdlib.h>
+
+using namespace std;
+
+// Displays the usage of the Present Simple Tense
+void displayTheUsageOfPresentSimpleTense(string mutualChoice);
+
+// Practise Present Simple Tense
+void practisePresentSimpleTenseEx1(string mutualChoice, int& points);
+
+// Practise Present Simple Tense
+void practisePresentSimpleTenseEx2(string mutualChoice, int& points);
+
+// Practise Present Simple Tense
+void practisePresentSimpleTenseEx3(string mutualChoice, int& points);
+
+// Practise Present Simple Tense
+void practisePresentSimpleTenseEx4(string mutualChoice, int& points);
+
+// Displays the usage of Present Continuous Tense
+void displayTheUsageOfPresentContinuousTense(string mutualChoice);
+
+// Practise Present Continuous Tense
+void practisePresentContinuousTenseEx1(string mutualChoice, int& points);
+
+// Practise Present Continuous Tense
+void practisePresentContinuousTenseEx2(string mutualChoice, int& points);
+
+// Practise Present Continuous Tense
+void practisePresentContinuousTenseEx3(string mutualChoice, int& points);
+
+// Practise Present Continuous Tense
+void practisePresentContinuousTenseEx4(string mutualChoice, int& points);
+
+// Displays the usage of Past Simple Tense
+void displayTheUsageOfPastSimpleTense(string mutualChoice);
+
+// Practise Past Simple Tense
+void practisePastSimpleTenseEx1(string mutualChoice, int& points);
+
+// Practise Past Simple Tense
+void practisePastSimpleTenseEx2(string mutualChoice, int& points);
+
+// Practise Past Simple Tense
+void practisePastSimpleTenseEx3(string mutualChoice, int& points);
+
+// Practise Past Simple Tense
+void practisePastSimpleTenseEx4(string mutualChoice, int& points);
+
+// Displays the usage of Past Continuous Tense
+void displayTheUsageOfPastContinuousTense(string mutualChoice);
+
+// Practise Past Continuous Tense
+void practisePastContinuousTenseEx1(string mutualChoice, int& points);
+
+// Practise Past Continuous Tense
+void practisePastContinuousTenseEx2(string mutualChoice, int& points);
+
+// Practise Past Continuous Tense
+void practisePastContinuousTenseEx3(string mutualChoice, int& points);
+
+// Practise Past Continuous Tense
+void practisePastContinuousTenseEx4(string mutualChoice, int& points);
+
+// Displays the usage of Present Perfect Tense
+void displayTheUsageOfPresentPerfectTense(string mutualChoice);
+
+// Practise Present Perfect Tense
+void practisePresentPerfectTenseEx1(string mutualChoice, int& points);
+
+// Practise Present Perfect Tense
+void practisePresentPerfectTenseEx2(string mutualChoice, int& points);
+
+// Practise Present Perfect Tense
+void practisePresentPerfectTenseEx3(string mutualChoice, int& points);
+
+// Practise Present Perfect Tense
+void practisePresentPerfectTenseEx4(string mutualChoice, int& points);
+
+// Displays the usage of Future Simple Tense
+void displayTheUsageOfFutureSimpleTense(string mutualChoice);
+
+// Practise Future Simple Tense
+void practiseFutureSimpleTenseEx1(string mutualChoice, int& points);
+
+// Practise Future Simple Tense
+void practiseFutureSimpleTenseEx2(string mutualChoice, int& points);
+
+// Practise Future Simple Tense
+void practiseFutureSimpleTenseEx3(string mutualChoice, int& points);
+
+// Practise Future Simple Tense
+void practiseFutureSimpleTenseEx4(string mutualChoice, int& points);
